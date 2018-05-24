11ac6231db3c67643bdf70a63c435a925699d3b0
AndyisBoss <34162605+AndyisBoss@users.noreply.github.com>
2018-05-24 08:29:26 -0400

    fix

--- core/src/com/compsciproject/game/Gun.java
+++ core/src/com/compsciproject/game/Gun.java
@@ -2,84 +2,21 @@ package com.compsciproject.game;
 
 public interface Gun 
 {
-<<<<<<< HEAD
-	
-	
-	
 	
 	public int getDmg();
 	
 	public void bulletLost();
 	
-	
 	public int getCoolDown();
 	
 	public void addCool();
-	public void coolDownWep();
 	
+	public void coolDownWep();
 	
 	public String getWeaponName();
 	
 	public int getAmmo();
 	
 	public void setCooldown();
-=======
-	private String name;
-	private int fireRate;
-	private int ammo;
-	protected int coolDown = 0;
-	private int dmg = 5;
-	private final int COOLDOWN = 5;
-	
-	public Gun(String name, int fireRate, int ammo)
-	{
-		this.name = name;
-		this.fireRate = fireRate;
-		this.ammo = ammo;
-	}
-	
-	public int getDmg() {	
-		return dmg;
-	}
-	
-	public void bulletLost()
-	{
-		ammo--;
-	}
-	
-	public int getFireRate()
-	{
-		return fireRate;
-	}
-	
-	public int getCoolDown() {
-		
-		return coolDown;
-	}
-	
-	public void addCool() {
-		coolDown++;
-	}
-	public void coolDownWep(){
-		if(coolDown > 0) {
-			coolDown--;
-			}
-		}
-	
-	
-	public String getWeaponName()
-	{
-		return name;
-	}
-	
-	public int getAmmo()
-	{
-		return ammo;
-	}
-	
-	public void setCooldown(){
-		coolDown += COOLDOWN;
-	}
->>>>>>> 752031d7b9ad3a0af091f8eb2adad9ca9161d595
 	
 }
--- core/src/com/compsciproject/game/Pistol.java
+++ core/src/com/compsciproject/game/Pistol.java
@@ -2,17 +2,11 @@ package com.compsciproject.game;
 
 public class Pistol implements Gun 
 {
-<<<<<<< HEAD
 	private String name;
 	private int ammo;
 	private int coolDown = 0;
 	private int dmg;
 	private final int COOLDOWN = 25;
-=======
-	private static String name = "M1911";
-	private final int COOLDOWN = 10; 
->>>>>>> 752031d7b9ad3a0af091f8eb2adad9ca9161d595
-	
 	public Pistol()
 	{
 		name = "M1911";
